2971bd2603ad4568eb40988af361e65f2ee6198c
silentbalanceyh <silentbalanceyh@126.com>
2023-06-29 06:11:02 +0800

    批量更新

--- src/utter/idyl.fn.rx.state.js
+++ src/utter/idyl.fn.rx.state.js
@@ -12,6 +12,8 @@ export default {
     rxClose: (reference) => (data = {}, addOn = {}) => {
         const updated = {};
         updated.$inited = data;
+        // 解决批量更新表单中无法刷新和加载的问题
+        updated.$loading = true;
         Object.assign(updated, addOn);
         return Ux.of(reference).in(updated).hide().future(() => Ux.promise(updated));
     }

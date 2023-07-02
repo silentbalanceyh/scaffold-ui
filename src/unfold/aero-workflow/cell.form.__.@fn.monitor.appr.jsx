0df0a5a637a4965926cd5d69537c9d1efbd4dae6
silentbalanceyh <silentbalanceyh@126.com>
2023-07-02 15:46:10 +0800

    History of Approval

--- src/unfold/aero-workflow/cell.form.__.@fn.monitor.appr.jsx
+++ src/unfold/aero-workflow/cell.form.__.@fn.monitor.appr.jsx
@@ -2,6 +2,62 @@ import React from 'react';
 import {Empty, Timeline} from "antd";
 import Ux from 'ux';
 
+const componentInit = (reference) => {
+    const {data = []} = reference.props;
+    if (0 === data.length) {
+        Ux.of(reference).ready().done();
+    } else {
+        const state = {};
+        const userId = data.map(item => item.createdBy);
+        Ux.ajaxPost("/api/user/search", {
+            criteria: {
+                "key,i": userId
+            }
+        }).then(users => {
+            const {list = []} = users;
+            const $lazy = {};
+            list.forEach(each => $lazy[each.key] = each.realname);
+            state.$lazy = $lazy;
+            Ux.of(reference).in(state).ready().done();
+            // reference.?etState(state);
+        })
+    }
+}
+
+class ApprovalComponent extends React.PureComponent {
+    componentDidMount() {
+        componentInit(this);
+    }
+
+    render() {
+        return Ux.xtRender(this, () => {
+            const {data = []} = this.props;
+            if (0 === data.length) {
+                return (<Empty/>)
+            } else {
+                const {$lazy = {}} = this.state;
+                const items = [];
+                data.forEach(history => {
+                    const item = {};
+                    item.key = history.key;
+                    const timeAt = Ux.valueDatetime(history['createdAt']);
+                    const name = $lazy[history.createdBy];
+                    item.children = (
+                        <>
+                            <p>{name}，{timeAt.format("YYYY-MM-DD HH:mm:ss")}</p>
+                            <p>{history.description}</p>
+                        </>
+                    );
+                    items.push(item);
+                })
+                return (
+                    <Timeline items={items}/>
+                )
+            }
+        });
+    }
+}
+
 export default (reference) => ($inited, config = {}) => {
     /*
      * $openAppr / $openFile
@@ -9,26 +65,5 @@ export default (reference) => ($inited, config = {}) => {
     const {history = []} = $inited;
     const approvals = history.filter(each => "APPROVAL" === each.type)
         .sort(Ux.sorterDescDFn('createdAt'));
-    if (0 === approvals.length) {
-        return (<Empty/>)
-    } else {
-        const {format} = config;
-        const items = [];
-        approvals.forEach(approval => {
-            const item = {};
-            item.key = approval.key;
-            const {recordNew = {}} = approval;
-            const timeAt = Ux.valueDatetime(approval['createdAt']);
-            item.children = (
-                <>
-                    <p>{recordNew['acceptedName']}, {timeAt.format(format)}</p>
-                    <p>{approval.description}</p>
-                </>
-            );
-            items.push(item);
-        })
-        return (
-            <Timeline items={items}/>
-        )
-    }
+    return (<ApprovalComponent data={approvals}/>);
 }

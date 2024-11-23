25ff567a28e8e479481cf2eee48c022b523408c1
silentbalanceyh <silentbalanceyh@126.com>
2024-11-23 08:02:01 +0800

    New server

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -76,7 +76,9 @@ module.exports = function (proxy, allowedHost) {
             // remove last slash so user can land on `/test` instead of `/test/`
             publicPath: paths.publicUrlOrPath.slice(0, -1),
         },
-        https: getHttpsConfig(),
+        server: {
+            type: getHttpsConfig() ? "https" : "http"
+        },
         host,
         historyApiFallback: {
             // Paths with dots should still use the history fallback.
@@ -86,7 +88,13 @@ module.exports = function (proxy, allowedHost) {
         },
         // `proxy` is run between `before` and `after` `webpack-dev-server` hooks
         proxy,
-        onBeforeSetupMiddleware(devServer) {
+        setupMiddlewares: (middlewares, devServer) => {
+            // 确保 devServer 存在
+            if (!devServer) {
+                throw new Error('webpack-dev-server is not running!');
+            }
+
+            // ==== onBeforeSetupMiddleware
             // Keep `evalSourceMapMiddleware`
             // middlewares before `redirectServedPath` otherwise will not have any effect
             // This lets us fetch source contents from webpack for the error overlay
@@ -96,17 +104,15 @@ module.exports = function (proxy, allowedHost) {
                 // This registers user provided middleware for proxy reasons
                 require(paths.proxySetup)(devServer.app);
             }
-        },
-        onAfterSetupMiddleware(devServer) {
-            // Redirect to `PUBLIC_URL` or `homepage` from `package.json` if url not match
-            devServer.app.use(redirectServedPath(paths.publicUrlOrPath));
 
-            // This service worker file is effectively a 'no-op' that will reset any
-            // previous service worker registered for the same host:port combination.
-            // We do this in development to avoid hitting the production cache if
-            // it used the same host and port.
-            // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-            devServer.app.use(noopServiceWorkerMiddleware(paths.publicUrlOrPath));
+            // ==== onAfterSetupMiddleware
+            // 添加重定向中间件
+            middlewares.push(redirectServedPath(paths.publicUrlOrPath));
+
+            // 添加 Service Worker 中间件
+            middlewares.push(noopServiceWorkerMiddleware(paths.publicUrlOrPath));
+
+            return middlewares;
         },
     };
     // Enable socket Yes/No status
